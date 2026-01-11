1db5c3643d56c18601e233fe17a977c31c020796
manondamour <manondamour2004@gmail.com>
2026-01-11 17:31:13 +0100

    analyse-insta.R

---
+++ analyse-insta.R
@@ -0,0 +1,11 @@
+# Installation des packages (si nécessaire)
+install.packages("tidyverse")
+install.packages("igraph")
+install.packages("rvest")
+install.packages("lubridate")
+
+#chargement des packages 
+library(tidyverse)
+library(igraph)
+library(rvest)
+library(lubridate)
